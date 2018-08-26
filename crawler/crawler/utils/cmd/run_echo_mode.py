f48a2dd447f603b654949b96a04d8b9fe70c704b
scorpionipx <scorpionipx@gmail.com>
2018-08-26 20:44:23 +0300

    start crawler host with args

--- crawler/crawler/utils/cmd/run_echo_mode.py
+++ crawler/crawler/utils/cmd/run_echo_mode.py
@@ -1,8 +1,35 @@
+from argparse import ArgumentParser
+
 from crawler.core import Crawler
 
 
 def main():
-    c = Crawler()
+    ap = ArgumentParser()
+
+    ap.add_argument("-i", "--ip_address",
+                    type=str,
+                    default=None,
+                    required=False,
+                    dest="ip_address",
+                    help="Crawler's IP address",
+                    metavar="IP_ADDRESS", action="store",
+                    )
+
+    ap.add_argument("-p", "--port",
+                    type=str,
+                    default=None,
+                    required=False,
+                    dest="port",
+                    help="Crawler's connection port",
+                    metavar="PORT", action="store",
+                    )
+
+    options = ap.parse_args()
+
+    ip = options.ip_address
+    port = options.port
+
+    c = Crawler(ip=ip, port=int(port))
     c.echo()
 
 
